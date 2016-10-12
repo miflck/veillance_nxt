91296a9c549cdf2130683525e027aca797975e28
miflck <contact@michaelflueckiger>
2016-10-12 18:05:33 +0200

    caroussel event

---
+++ src/CarousselEvent.cpp
@@ -0,0 +1,9 @@
+//
+//  CarousselEvent.cpp
+//  TypeStudy_02
+//
+//  Created by Flückiger Michael on 12.10.16.
+//
+//
+
+#include "CarousselEvent.hpp"
---
+++ src/CarousselEvent.hpp
@@ -0,0 +1,14 @@
+//
+//  CarousselEvent.hpp
+//  TypeStudy_02
+//
+//  Created by Flückiger Michael on 12.10.16.
+//
+//
+
+#ifndef CarousselEvent_hpp
+#define CarousselEvent_hpp
+
+#include <stdio.h>
+
+#endif /* CarousselEvent_hpp */
