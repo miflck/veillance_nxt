d9ecc6da9fd4a22dadbf50775765ca4e4ccdfcb3
miflck <contact@michaelflueckiger>
2016-11-02 13:46:34 +0100

    fragment

---
+++ src/Fragment.cpp
@@ -0,0 +1,9 @@
+//
+//  Fragment.cpp
+//  TypeStudy_02
+//
+//  Created by Flückiger Michael on 01.11.16.
+//
+//
+
+#include "Fragment.hpp"
---
+++ src/Fragment.hpp
@@ -0,0 +1,14 @@
+//
+//  Fragment.hpp
+//  TypeStudy_02
+//
+//  Created by Flückiger Michael on 01.11.16.
+//
+//
+
+#ifndef Fragment_hpp
+#define Fragment_hpp
+
+#include <stdio.h>
+
+#endif /* Fragment_hpp */
